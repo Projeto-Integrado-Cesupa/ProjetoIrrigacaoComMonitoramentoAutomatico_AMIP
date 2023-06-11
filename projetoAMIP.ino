fe96312b0914d809381d58ec9968d09eac5f7b3b
Liane Ferreira Heidemann <lianeheidemann@gmail.com>
2023-06-10 22:20:57 -0300

    Update projetoAMIP.ino

--- projetoAMIP.ino
+++ projetoAMIP.ino
@@ -9,26 +9,31 @@ DFRobot_DHT11 DHT;
 
 String mensagem1;
 String mensagem2;
+String mensagemX;
+int x1;
+int x2;
+int x3;
 
 void setup(){  
   lcd.init();
-  lcd.backlight();
+	lcd.backlight();
   pinMode(PIN_SensorUmidadeSolo, INPUT);
-  Serial.begin(9600);  
-}
+;	Serial.begin(9600);  
+;  }
  
 void loop(){ 
   temperatura(); 
   umidadeDoAr();
   display();  
-  Serial.println(""); 
   umidadeDaTerra(); 
-  display();    
+  display();   
+  bluetoohMensagem();
 }
 
 void umidadeDaTerra(){  
   int umidade = analogRead(PIN_SensorUmidadeSolo);
   int porcento = map(umidade, 0, 1023, 0, 100);
+  x3 = porcento;
   mensagem1 ="Umidade do Solo:";
   mensagem2 = String(porcento)+"%";
 }
@@ -36,6 +41,7 @@ void umidadeDaTerra(){
 void umidadeDoAr() {
   DHT.read(DHT11_PIN);
   int h = DHT.humidity;
+  x1 = h;
   mensagem1 = "Umidade do Ar: ";
   mensagem2 = String(h) + "%";
 }
@@ -43,6 +49,7 @@ void umidadeDoAr() {
 void temperatura() {
   DHT.read(DHT11_PIN);
   int t = DHT.temperature;
+  x2 = t;
   mensagem2 = String(t);
   lcd.setCursor(0,0); 
   lcd.print("Temperatura:"); 
@@ -56,9 +63,16 @@ void temperatura() {
 
 void display() {
   lcd.setCursor(0,0);          
-  lcd.print(mensagem1); 
+  lcd.print(mensag/em1); 
   lcd.setCursor(0,1);           
   lcd.print(mensagem2);
   delay(3000);
   lcd.clear();
+}
+
+void bluetoohMensagem(){
+  DHT.read(DHT11_PIN);
+  mensagemX = (String) "[ "+ x1 +"% ]   [ "+ x2 +"°C ]   [ " + x3 +"% ]";
+  Serial.println(mensagemX);
+  delay(2000);
 }
